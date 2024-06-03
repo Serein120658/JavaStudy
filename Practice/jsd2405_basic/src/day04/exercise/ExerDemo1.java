a67cb96b5dda85db77df12e96c545da05792b850
Serein <3152280792@qq.com>
2024-06-03 18:04:47 +0800

    标准练习1的完成

--- Practice/jsd2405_basic/src/day04/exercise/ExerDemo1.java
+++ Practice/jsd2405_basic/src/day04/exercise/ExerDemo1.java
@@ -1,7 +1,25 @@
 package day04.exercise;
 
+import java.util.Scanner;
+
 public class ExerDemo1 {
     public static void main(String[] args) {
-        // 练习1
+        /*
+        * 1.	CommandBySwitch命令解析程序：
+                要求：接收用户输入的命令，依据命令做不同的操作
+        * */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入你的选择:1.登录，2.注册");
+        int num = sc.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("登录");
+                break;
+            case 2:
+                System.out.println("注册");
+                break;
+            default:
+                System.out.println("输入有误");
+        }
     }
 }
