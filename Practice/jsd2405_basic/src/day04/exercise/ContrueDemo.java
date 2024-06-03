235f7b0ae1a5c7189624721e0ebaad1bd52a15e5
Serein <3152280792@qq.com>
2024-06-03 20:58:01 +0800

    串讲小练习

---
+++ Practice/jsd2405_basic/src/day04/exercise/ContrueDemo.java
@@ -0,0 +1,18 @@
+package day04.exercise;
+
+public class ContrueDemo {
+    public static void main(String[] args) {
+        // 求出1-100所有偶数的和
+        int sum = 0,sum1=0;
+        for(int i = 1; i <= 100; i++){
+            if(i%2 == 0){
+                // System.out.print(i + "  ");
+                sum +=i;
+            }else{
+                sum1 += i;
+            }
+        }
+        System.out.println(sum);
+        System.out.println(sum1);
+    }
+}
