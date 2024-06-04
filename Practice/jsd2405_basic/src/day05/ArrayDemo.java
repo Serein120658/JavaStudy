f9a6479bd0af5a1486c0f0b81f38b29ca03be9a0
Serein <3152280792@qq.com>
2024-06-04 11:39:15 +0800

    数组初始化

--- Practice/jsd2405_basic/src/day05/ArrayDemo.java
+++ Practice/jsd2405_basic/src/day05/ArrayDemo.java
@@ -15,5 +15,19 @@ public class ArrayDemo {
         double [] b = new double[10];
         // 声明了布尔型数组c，包含了20个元素，每个元素都是boolean类型，默认值为false
         boolean [] c = new boolean[20];
+        // 声明了字符型数组h，包含3个元素，每个元素都是char类型，默认值为0
+        char[] h = new char[3];
+        for (char value : h) {
+            System.out.println(value);
+//            System.out.println((char) 0);
+        }
+        // 数组初始化,初始化数组当中的元素
+        int[] d = new int[3]; // 0,0,0
+        int[] e = {1,2,3}; // 1,2,3
+        int[] f = new int[]{1,2,3}; // 1,2,3
+        int[] g;
+        //  g = {1,2,3,};  // 编译错误，此方式只能声明，同时初始化
+        g = new int[]{1,2,3};
+
     }
 }
