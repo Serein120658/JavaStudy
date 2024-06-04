1a2a1e62ffff49477d3e18c28ecf731ebb010cda
Serein <3152280792@qq.com>
2024-06-04 11:18:00 +0800

    数组定义

---
+++ Practice/jsd2405_basic/src/day05/ArrayDemo.java
@@ -0,0 +1,19 @@
+package day05;
+
+/**
+ * 什么是数组？
+ * 数组是一种引用类型，数组中存放的是同一种数据类型的元素
+ *
+ */
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        // 数组的定义
+        // 声明了整型数组a，包含5个元素，每个元素都是int类型，默认值为0
+        int [] a = new int[5];
+        // 声明了浮点型数组b,包含了10个元素，每个元素都是double类型，默认值为0.0
+        double [] b = new double[10];
+        // 声明了布尔型数组c，包含了20个元素，每个元素都是boolean类型，默认值为false
+        boolean [] c = new boolean[20];
+    }
+}
