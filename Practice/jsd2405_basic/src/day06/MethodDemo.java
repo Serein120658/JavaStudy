3c256ba0c61ac8c791e1eacda473e46d340e245b
Serein <3152280792@qq.com>
2024-06-05 11:39:14 +0800

    方法的练习

--- Practice/jsd2405_basic/src/day06/MethodDemo.java
+++ Practice/jsd2405_basic/src/day06/MethodDemo.java
@@ -1,5 +1,7 @@
 package day06;
 
+import java.util.Arrays;
+
 /**
  * 方法的练习
  */
@@ -13,6 +15,10 @@ public class MethodDemo {
 
         // 有参，有返回值方法调用
         System.out.println(makeMarriage("阿尔特曼","维丽娜"));
+
+        // 有参，有返回值方法调用，返回值为数组
+        String[] arr = makeArray("小李",20,'男',3);
+        System.out.println(Arrays.toString(arr));
 
     }
 
@@ -31,5 +37,15 @@ public class MethodDemo {
     // 求婚
     public static String makeMarriage(String male,String female) {
         return (male + "正在向" + female + "求婚");
+    }
+    // 返回值为String数组
+    public static String[] makeArray(String a,int b,char c,int length) {
+        String[] str = new String[length];
+        // 使用循环去为数组补充内容
+        for (int i = 0; i < length; i++) {
+            str[i] = "我叫" + a + "，今年" + b +"岁了" +"性别:" + c;
+        }
+
+        return str;
     }
 }
