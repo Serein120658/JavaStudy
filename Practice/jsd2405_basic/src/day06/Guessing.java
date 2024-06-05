54d5c22eafff2f43b017c4b9493eb80398c256f0
Serein <3152280792@qq.com>
2024-06-05 17:09:29 +0800

    猜数小游戏(死循环版)

--- Practice/jsd2405_basic/src/day06/Guessing.java
+++ Practice/jsd2405_basic/src/day06/Guessing.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 
 public class Guessing {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Random random = new Random();
         int num = random.nextInt(100);
+        int input;
         while (true) {
-            Scanner scanner = new Scanner(System.in);
+
             System.out.println("请输入一个数字");
-            int num1 = scanner.nextInt();
-            if (num1 > num) {
+            input = sc.nextInt();
+            if (input > num) {
                 System.out.println("猜大了");
-            } else if (num1 < num) {
+            } else if (input < num) {
                 System.out.println("猜小了");
             } else {
                 System.out.println("猜对了");
