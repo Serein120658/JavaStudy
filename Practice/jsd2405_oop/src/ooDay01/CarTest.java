0cc74e0eb7e260cb7db933bc74f5b540f7267df8
Serein <3152280792@qq.com>
2024-06-06 16:50:20 +0800

    汽车类测试

---
+++ Practice/jsd2405_oop/src/ooDay01/CarTest.java
@@ -0,0 +1,10 @@
+package ooDay01;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car car = new Car("劳斯莱斯","黑曜石",9999999.99);
+        car.start();
+        car.run();
+        car.stop();
+    }
+}
