03880570b444ea3520621fba8470887f2f5d6ca3
Serein <3152280792@qq.com>
2024-06-06 10:34:24 +0800

    学生实例化测试

---
+++ Practice/jsd2405_oop/src/ooDay01/StudentTest.java
@@ -0,0 +1,14 @@
+package ooDay01;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        // 创建学生的实例化对象
+        Student zg = new Student("张光", 18, "jsd2405班", "2020001");
+        zg.sayHi();
+        zg.playWith("李光");
+        zg.study();
+
+
+
+    }
+}
