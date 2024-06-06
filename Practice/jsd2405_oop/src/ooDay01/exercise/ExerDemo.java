987345a24b63fbfdd589a07e44c3cac18b416d34
Serein <3152280792@qq.com>
2024-06-06 19:40:51 +0800

    oopDay01串讲练习

---
+++ Practice/jsd2405_oop/src/ooDay01/exercise/ExerDemo.java
@@ -0,0 +1,13 @@
+package ooDay01.exercise;
+
+public class ExerDemo {
+    public static void main(String[] args) {
+        int row = 5;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
