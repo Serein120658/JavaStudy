d480eaa105c47fc5ce2c83524fae9d4c52916d1a
Serein <3152280792@qq.com>
2024-06-11 10:29:15 +0800

    interface接口的练习

---
+++ Practice/jsd2405_oop/src/ooDay03/Swim.java
@@ -0,0 +1,12 @@
+package ooDay03;
+
+/**
+ * 接口是一种引用数据类型
+ * 有interface定义
+ * 接口当中的方法默认都是抽象的，前面默认有abstract
+ * 接口当中的变量默认都是常量，前面默认有final
+ */
+
+public interface Swim {
+    void  swim();  // 相当于 abstract void swim(); 注意没有方法体,子类继承的接口必须实现接口中的方法
+}
