f4f6519548cd715e65e8c6fb1bddcf3e4261f2c1
Serein <3152280792@qq.com>
2024-06-11 15:42:47 +0800

    定义引用类型数组

---
+++ Practice/jsd2405_oop/src/ooDay03/RefArrayDemo.java
@@ -0,0 +1,18 @@
+package ooDay03;
+
+/**
+ * 引用类型数组
+ */
+public class RefArrayDemo {
+    public static void main(String[] args) {
+        // 声明Dog型数组dogs，包含三个元素，每个元素都为dog型，默认值为null
+        Dog[] dogs = new Dog[3];
+
+
+        //声明Chick型数组chick，包含三个元素，每个元素都为chick型，默认值为null
+        Chick[] chicks = new Chick[3];
+
+        //声明Fish型数组fish，包含三个元素，每个元素都为fish型，默认值为null
+        Fish[] fishs = new Fish[3];
+    }
+}
