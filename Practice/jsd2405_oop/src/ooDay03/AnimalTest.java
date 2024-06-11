5261adcbc4459cc36a8a1024a38747bf3f5999ad
Serein <3152280792@qq.com>
2024-06-11 09:57:45 +0800

    abstract抽象类的练习

---
+++ Practice/jsd2405_oop/src/ooDay03/AnimalTest.java
@@ -0,0 +1,24 @@
+package ooDay03;
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        //
+        // 创建狗类
+        Dog dog = new Dog("旺财", 3, "黄色");
+        dog.lookHome();
+        dog.eat();
+        dog.drink();
+
+        // 创建鸡类 名叫爱坤 年龄22 颜色背带裤
+   Chick chick = new Chick("爱坤", 22, "背带裤");
+        chick.layEggs();
+        chick.eat();
+        chick.drink();
+
+        // 创建🐟  名叫波妞 年龄3 颜色粉白
+        Fish fish = new Fish("波妞", 3, "粉白");
+        fish.eat();
+        fish.drink();
+
+    }
+}
---
+++ Practice/jsd2405_oop/src/ooDay03/Dog.java
@@ -0,0 +1,24 @@
+package ooDay03;
+
+public class Dog extends Animal {
+    // 设计当中没有添加属性
+
+    Dog(){}
+
+    // 有参
+    Dog(String name, int age, String color){
+        super(name,age,color);
+    }
+
+    // lookHome
+    void lookHome(){
+        System.out.println(color + "颜色的" + age +"岁的狗狗" + name + "正在看家");
+    }
+
+    // eat重写
+    @Override
+    void eat(){
+        System.out.println(color + "颜色的" + age +"岁的狗狗" + name + "正在吃骨头");
+    }
+
+}
