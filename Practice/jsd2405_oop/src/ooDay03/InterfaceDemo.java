5857dcdb2c8fbaccbdf7ab99bea9027665b0496f
Serein <3152280792@qq.com>
2024-06-11 11:39:38 +0800

    interface接口实现

--- Practice/jsd2405_oop/src/ooDay03/InterfaceDemo.java
+++ Practice/jsd2405_oop/src/ooDay03/InterfaceDemo.java
@@ -1,5 +1,26 @@
 package ooDay03;
 
+// 两个测试接口
+interface Inter1{
+    static final int MAX_NUM = 100;
+    void show();
+}
+interface Inter2{
+    void show2();
+}
+// 创建一个测试类去实现接口
+class InterfaceTest implements Inter1,Inter2 {
+    // 重写
+    public void show(){
+        System.out.println("show");
+    }
+    // 重写
+    public void show2(){
+        System.out.println("show2");
+    }
+
+}
+
 public class InterfaceDemo {
     public static void main(String[] args){
         // 接口不能被实例化,也就是不能被new
@@ -12,5 +33,10 @@ public class InterfaceDemo {
             }
         };
         inter.show();
+
+        // 创建测试实例
+        InterfaceTest test = new InterfaceTest();
+        test.show();
+        test.show2();
     }
 }
