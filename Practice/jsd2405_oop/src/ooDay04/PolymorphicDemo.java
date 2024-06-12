94b1c0f18e4d23386adf0163185ca556e2834080
Serein <3152280792@qq.com>
2024-06-12 11:03:50 +0800

    转型实例

--- Practice/jsd2405_oop/src/ooDay04/PolymorphicDemo.java
+++ Practice/jsd2405_oop/src/ooDay04/PolymorphicDemo.java
@@ -4,8 +4,8 @@ package ooDay04;
  * 多态演示
  * 向上造型
  * 超类型的引用类型指向子类对象
- * 应用1：将超类作为参数类型，传递派生类对象    以扩大方法的应用范围   实现代码复用
- * 应用2：将超类作为返回类型，返回派生类对象
+ * 应用1：将超类作为参数类型或者返回值类型，传递派生类对象    以扩大方法的应用范围   实现代码复用
+ * 应用2：
  * 向下造型
  */
 public class PolymorphicDemo {
@@ -39,5 +39,38 @@ public class PolymorphicDemo {
         // 在传参的同时，系统自动向上造型。对于feed方法，Animal animal = a4;
         Dog a4 = new Dog("小黑", 2, "黑色");
         m1.feed(a4);
+
+
+        // 向上造型第二点应用
+        // 创建animals数组对象
+        Animal[] animals = new Animal[3];
+        animals[0] = new Dog("小黑", 2, "黑色");  // 向上造型
+        animals[1] = new Chick("坤哥",24,"背带裤"); // 向上造型
+        animals[2] = new Fish("鱼", 1, "青色"); // 向上造型
+        for (int i = 0; i < animals.length; i++) { // 遍历所有的Animal
+            System.out.println(animals[i].name);
+            m1.feed(animals[i]);
+        }
+
+        // 向下造型
+        for(int i = 0; i < animals.length; i++){
+            if(animals[i] instanceof Dog){
+                // 强制转换
+                Dog dog = (Dog) animals[i];
+                // 此时就可以调用dog类里面的方法或者其他属性.例如
+                dog.lookHome();
+                dog.swim();
+            }
+            if(animals[i] instanceof Chick){
+                // 强制转换
+                Chick chick = (Chick) animals[i];
+                chick.layEggs();
+            }
+            if(animals[i] instanceof Fish){
+                // 强制转换
+                Fish fish = (Fish) animals[i];
+                fish.swim();
+            }
+        }
     }
 }
