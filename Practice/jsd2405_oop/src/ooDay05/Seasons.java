040ba1f628215df12c0e78b9c34d17c0e0f7a771
Serein <3152280792@qq.com>
2024-06-13 15:54:38 +0800

    简化版季节枚举

---
+++ Practice/jsd2405_oop/src/ooDay05/Seasons.java
@@ -0,0 +1,14 @@
+package ooDay05;
+
+/**
+ * 简化版季节枚举
+ * 第一行必须罗列所有的枚举常量
+ */
+public enum Seasons {
+    // public static final Seasons SPRING = new Seasons();
+    // public static final Seasons SUMMER = new Seasons();
+    // public static final Seasons AUTUMN = new Seasons();
+    // public static final Seasons WINTER = new Seasons();
+    SPRING,SUMMER,AUTUMN,WINTER;  // 表示Seasons的固定的4个对象，都是常量
+
+}
