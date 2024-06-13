a428865b6bee77b1433f2c745ad0b58034fe792a
Serein <3152280792@qq.com>
2024-06-13 10:22:16 +0800

    getter和setter(JavaBean规范)

--- Practice/jsd2405_oop/src/ooDay05/Point.java
+++ Practice/jsd2405_oop/src/ooDay05/Point.java
@@ -3,8 +3,11 @@ package ooDay05;
 /**
  * 演示访问权限
  * 标准JavaBean的规范：
- * 1、成员变量要私有，私有的成员变量要提供getter和setter方法
+ * 1、成员变量要私有，私有的成员变量要提供getter和setter方法，没有它就获取不到数据
+ * 可以将设计getter和setter理解为一种行业标准
  * 2、每个类里面都有一个公开的无参的构造方法
+ * 3、可以更好的保证数据的合法性(因为方法中可以做条件控制)
+ * 4、getter和setter可以选择性存在(只有getter(只读)，或者只有setter(只写))
  */
 public class Point {
     private int x;
