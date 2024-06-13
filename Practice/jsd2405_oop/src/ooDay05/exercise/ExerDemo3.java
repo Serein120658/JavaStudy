acbc3ae1f2218c4fe73571d41ef4c15c81eafea2
Serein <3152280792@qq.com>
2024-06-13 20:13:23 +0800

    标准练习3

---
+++ Practice/jsd2405_oop/src/ooDay05/exercise/ExerDemo3.java
@@ -0,0 +1,13 @@
+package ooDay05.exercise;
+
+/**
+ * 3. 完成StaticFinalDemo，演示static final常量的用法
+ */
+public class ExerDemo3 {
+    public static void main(String[] args) {
+        // 常量练习
+        // 类名.常量名访问
+        System.out.println(StaticFinal.PI);
+        System.out.println(StaticFinal.NUM);
+    }
+}
---
+++ Practice/jsd2405_oop/src/ooDay05/exercise/StaticFinal.java
@@ -0,0 +1,11 @@
+package ooDay05.exercise;
+
+public class StaticFinal {
+    // 常量，使用static final 修饰
+    // 1. 常量必须赋值
+    // 常量使用大写字母，如果命名多个单词，使用下划线连接
+    static final int NUM = 20;
+    static final double PI = 3.1415926;
+
+
+}
