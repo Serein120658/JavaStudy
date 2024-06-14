c5041fa69d8adb08f04d23b42523905bdce1667a
Serein <3152280792@qq.com>
2024-06-14 10:15:10 +0800

    文档注释/**  /

---
+++ Practice/jsd2405_API/src/main/java/apidoc/DocDemo.java
@@ -0,0 +1,12 @@
+package apidoc;
+
+/**
+ * 文档注释示例
+ * @author serein
+ * @date 2024-06-14 10:14
+ */
+public class DocDemo {
+    public static void main(String[] args) {
+        System.out.println("文档注释!");
+    }
+}
